892a161703f0fe99c80a7875e84edaa668a881e4
Romesh McCullough <romesh.mccullough@gmail.com>
2017-02-27 12:30:08 -0500

    also use custom host header if provided

--- airflow/www/proxy.py
+++ airflow/www/proxy.py
@@ -8,6 +8,7 @@ class ReverseProxied(object):
     location /myprefix {
         proxy_pass http://192.168.0.1:5001;
         proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-Host $host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Scheme $scheme;
         proxy_set_header X-Script-Name /myprefix;
@@ -29,5 +30,9 @@ class ReverseProxied(object):
         scheme = environ.get('HTTP_X_SCHEME', '')
         if scheme:
             environ['wsgi.url_scheme'] = scheme
+
+        server = environ.get('HTTP_X_FORWARDED_HOST', '')
+        if server:
+            environ['HTTP_HOST'] = server
 
         return self.app(environ, start_response)
